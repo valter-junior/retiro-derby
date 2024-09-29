2a2d2eaa9053b706c313b1bea7399063e4f8d921
valterjunior <featcode@mercadocar.com.br>
2024-09-29 14:37:21 -0300

    Success page added.

--- google-sheet.js
+++ google-sheet.js
@@ -1,15 +1,29 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbzKK5VdL8R6l9Ioy_zcCsjZ8ofCfNRPGN6HW9EtdAXW9cekYFSpddAcNMu1NZk3FwkpYQ/exec";
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzKK5VdL8R6l9Ioy_zcCsjZ8ofCfNRPGN6HW9EtdAXW9cekYFSpddAcNMu1NZk3FwkpYQ/exec";
 
 const form = document.forms["subscription-form"];
+const loadingElement = document.getElementById("loading");
+const successElement = document.getElementById("success-message");
+const backButton = document.getElementById("back-button");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  loadingElement.style.display = "flex";
+
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) =>
-      alert("Thank you! your form is submitted successfully.")
-    )
-    .then(() => {
-      window.location.reload();
+    .then((response) => {
+      loadingElement.style.display = "none";
+      successElement.style.display = "flex";
     })
-    .catch((error) => console.error("Error!", error.message));
+    .catch((error) => {
+      console.error("Error!", error.message);
+      loadingElement.style.display = "none";
+    });
+});
+
+backButton.addEventListener("click", () => {
+  successElement.style.display = "none";
+  form.reset();
+  window.location.reload();
 });
