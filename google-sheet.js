aae3e231f7f66ee51f16bbca2f2eb77c88bdb5d4
valterjunior <featcode@mercadocar.com.br>
2024-10-03 13:08:50 -0300

    form reset added.

--- google-sheet.js
+++ google-sheet.js
@@ -3,73 +3,73 @@ const scriptURL =
 
 let isSubmitting = false;
 
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.forms["subscription-form"];
-  const loadingElement = document.getElementById("loading");
-  const successElement = document.getElementById("success-message");
-  const backButton = document.getElementById("back-button");
-  const copyPixButton = document.getElementById("copy-pix-button");
-  const pixCodeInput = document.getElementById("pix-code");
-  const pixTooltip = document.getElementById("pix-tooltip");
-  const submitButton = document.getElementById("submit");
+const form = document.forms["subscription-form"];
+const loadingElement = document.getElementById("loading");
+const successElement = document.getElementById("success-message");
+const backButton = document.getElementById("back-button");
+const copyPixButton = document.getElementById("copy-pix-button");
+const pixCodeInput = document.getElementById("pix-code");
+const pixTooltip = document.getElementById("pix-tooltip");
+const submitButton = document.getElementById("submit");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    document.getElementById("otherChurchOption").value =
-      document.getElementById("otherChurch").value;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  document.getElementById("otherChurchOption").value =
+    document.getElementById("otherChurch").value;
 
-    if (isSubmitting) return;
-    isSubmitting = true; // Lock the form for submission
+  if (isSubmitting) return;
+  isSubmitting = true; // Lock the form for submission
 
-    submitButton.disabled = true;
-    loadingElement.style.display = "flex";
+  submitButton.disabled = true;
+  loadingElement.style.display = "flex";
 
-    fetch(scriptURL, { method: "POST", body: new FormData(form) })
-      .then((response) => {
-        loadingElement.style.display = "none";
-        successElement.style.display = "flex";
-        isSubmitting = false;
-      })
-      .catch((error) => {
-        console.error("Error!", error.message);
-        loadingElement.style.display = "none";
-        isSubmitting = false; // Unlock form submission
-        submitButton.disabled = false;
-      });
-  });
-
-  backButton.addEventListener("click", () => {
-    successElement.style.display = "none";
-    window.location.reload();
-  });
-
-  document
-    .getElementById("youtube-redirect")
-    .addEventListener("click", function () {
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      loadingElement.style.display = "none";
+      successElement.style.display = "flex";
+      isSubmitting = false;
       form.reset();
-      window.location.href = "https://youtu.be/ppX9OPxq6Qw?si=5YlDx_f9MnKDeyir";
+    })
+    .catch((error) => {
+      console.error("Error!", error.message);
+      loadingElement.style.display = "none";
+      isSubmitting = false; // Unlock form submission
+      submitButton.disabled = false;
+      form.reset();
     });
+});
 
-  copyPixButton.addEventListener("click", () => {
-    pixCodeInput.select();
-    pixCodeInput.setSelectionRange(0, 99999);
+backButton.addEventListener("click", () => {
+  successElement.style.display = "none";
+  window.location.reload();
+});
 
-    navigator.clipboard
-      .writeText(pixCodeInput.value)
-      .then(() => {
-        pixTooltip.style.display = "block";
-        pixTooltip.classList.add("show-tooltip");
-
-        // Hide the tooltip after 2 seconds
-        setTimeout(() => {
-          pixTooltip.classList.remove("show-tooltip");
-          setTimeout(() => {
-            pixTooltip.style.display = "none";
-          }, 300); // wait for transition to end before hiding
-        }, 2000);
-      })
-      .catch((error) => {
-        console.error("Erro ao copiar o código PIX:", error);
-      });
+document
+  .getElementById("youtube-redirect")
+  .addEventListener("click", function () {
+    form.reset();
+    window.location.href = "https://youtu.be/ppX9OPxq6Qw?si=5YlDx_f9MnKDeyir";
   });
+
+copyPixButton.addEventListener("click", () => {
+  pixCodeInput.select();
+  pixCodeInput.setSelectionRange(0, 99999);
+
+  navigator.clipboard
+    .writeText(pixCodeInput.value)
+    .then(() => {
+      pixTooltip.style.display = "block";
+      pixTooltip.classList.add("show-tooltip");
+
+      // Hide the tooltip after 2 seconds
+      setTimeout(() => {
+        pixTooltip.classList.remove("show-tooltip");
+        setTimeout(() => {
+          pixTooltip.style.display = "none";
+        }, 300); // wait for transition to end before hiding
+      }, 2000);
+    })
+    .catch((error) => {
+      console.error("Erro ao copiar o código PIX:", error);
+    });
 });
